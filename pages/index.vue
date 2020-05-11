b6f1e16c63dfbeaf04e94631b423af0649d5d6de
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-05-11 18:15:58 +0530

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h2>Welcome to the best dad jokes app on the web</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default {
+  head() {
+    return {
+      title: 'DadJokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'The best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
